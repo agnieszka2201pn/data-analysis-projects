d2307eaaf5138d39f0ff39e3241e784b98fe4ad5
Agnieszka <agnieszka.blicharska@gmail.com>
2021-05-17 20:50:08 +0200

    17 maja

---
+++ python1_2/zadanie 1.py
@@ -0,0 +1,7 @@
+import random
+
+no_of_stars = random.randint(1,20)
+print("Wylosowana liczba wynosi: ", no_of_stars)
+stars = "*" * no_of_stars
+print(stars)
+
---
+++ python1_2/zadanie 2.py
@@ -0,0 +1,4 @@
+import random
+rows = random.randint(5, 15)
+columns = random.randint(5, 15)
+
