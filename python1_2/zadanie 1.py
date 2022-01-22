2be9f0585441be93f0014402faae129beeab6722
agnieszka2201pn <79762172+agnieszka2201pn@users.noreply.github.com>
2022-01-22 10:23:49 +0100

    Delete python1_2 directory

--- python1_2/zadanie 1.py
+++
@@ -1,7 +0,0 @@
-import random
-
-no_of_stars = random.randint(1,20)
-print("Wylosowana liczba wynosi: ", no_of_stars)
-stars = "*" * no_of_stars
-print(stars)
-
--- python1_2/zadanie 2.py
+++
@@ -1,4 +0,0 @@
-import random
-rows = random.randint(5, 15)
-columns = random.randint(5, 15)
-
